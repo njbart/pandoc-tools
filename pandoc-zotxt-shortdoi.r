9bc8dbbea90a04cdd86d17ced270586ae618ceb5
Nick Bart <njbart@users.noreply.github.com>
2017-05-05 13:31:13 +0000

    Fix due to changes in pandoc’s native AST/JSON

--- pandoc-zotxt-shortdoi.r
+++ pandoc-zotxt-shortdoi.r
@@ -1,7 +1,7 @@
 #! /usr/bin/env Rscript
 
 # Replaces any DOI in pandoc-zotxt’s output by a shortDOI (see http://shortdoi.org/).
-# Requires R (https://www.r-project.org/).
+# Requires R (https://www.r-project.org/), jq (https://stedolan.github.io/jq/), and curl
 # Usage: pandoc -s --filter pandoc-zotxt --filter pandoc-zotxt-shortdoi.r --filter pandoc-citeproc myfile.md
 
 library("methods")
@@ -12,13 +12,13 @@ data <- scan(file="stdin", what=character(0), quiet=TRUE)
 
 astr <- fromJSON(data, simplifyVector = FALSE, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
 
-bibfile <- astr[[1]]$unMeta$bibliography$c[[1]]$c
+bibfile <- astr$meta$bibliography$c[[1]]$c
 
 bibdata <- fromJSON(bibfile, simplifyVector = FALSE, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
 
 for (n in seq_along(bibdata)) {
 	if (exists('DOI', where=bibdata[[n]])) {
-		getshortdoi <- paste0("curl -sS \"http://shortdoi.org/", bibdata[[n]]$DOI, "?format=json\" | jq -r .ShortDOI")
+		getshortdoi <- paste0("curl -sS --globoff \"http://shortdoi.org/", bibdata[[n]]$DOI, "?format=json\" | jq -r .ShortDOI")
 		shortdoi <- try(system(getshortdoi, intern = TRUE))
 			if (identical(shortdoi, character(0))) {
 				bibdata[[n]]$DOI <- "✖︎︎︎✖︎︎︎✖︎︎︎ FIXME: INVALID DOI ✖︎︎︎✖︎︎︎✖︎︎︎"
